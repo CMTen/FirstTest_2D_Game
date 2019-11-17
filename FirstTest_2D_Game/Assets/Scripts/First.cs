761ec600a86d9221e272c448ae11066c83415046
CMTen <57666274+CMTen@users.noreply.github.com>
2019-11-18 00:15:28 +0800

    添加腳本與欄位

--- FirstTest_2D_Game/Assets/Scripts/First.cs
+++ FirstTest_2D_Game/Assets/Scripts/First.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;  //引用Unity API
 
+//類別 類別名稱
 public class First : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 定義欄位 Field(宣告變數)
+    // 語法:
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
+    // 私人 private  - 隱藏(預設)
+    // 公開 public   - 顯示
+    public int level = 1;         // 整數 預設 0
+    public float speed = 10.5f;   // 浮點數 預設 0
+    public string prop = "道具";  // 字串 預設 ""
+    public bool mission = true;  // 布林值(是/否) 預設 false
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int HP = 100;
+    public float Speed = 2.5f;
+    public string Character = "角色";
+
 }
